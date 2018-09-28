dd377217f743f8b7a68ee7807bbff48c1f33762e
Felix <felixgate13@gmail.com>
2018-09-28 16:58:09 +0100

    added links to dashboard, added form to products.php and finished form to about.php

---
+++ about.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title> About Page </title>
+
+</head>
+
+
+<body>
+
+
+<form action="login.php" method="post">
+
+    <input type="text" name="username" placeholder="Username">
+
+    <input type="text" name="password" placeholder="Password">
+
+    <br>
+
+    <input type="submit">
+
+</form>
+
+
+</body>
+
+
+
+</html>
---
+++ products.php
@@ -0,0 +1,50 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title> Project Page </title>
+
+</head>
+
+
+<body>
+
+
+<form action="login.php" method="post">
+    <select name="position">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+    </select>
+    <br>
+    picture_image_front
+    <input type="text" name="picture_url" placeholder="picture_url"><br>
+
+    picture_text
+    <input type="text" name="picture_text" placeholder="picture_text"><br>
+
+    app_link
+    <input type="text" name="app_link" placeholder="app_link"><br>
+
+    code_link
+    <input type="text" name="code_link" placeholder="code_link"><br>
+
+    <input type="submit">
+
+</form>
+
+
+</body>
+
+
+
+</html>
