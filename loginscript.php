7b3b1cd07606a80168d41ef534e26fec0a510b72
Felix <felixgate13@gmail.com>
2018-10-04 15:16:51 +0100

    changed hash

--- loginscript.php
+++ loginscript.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (password_verify($_POST['password'], '$2y$10$2GyC6.oj2u2dADpUtjl2CObSOQMMCrr1Yk4aGuyknS8oGI09pyU/a') && password_verify($_POST['username'], '$2y$10$rDQO8fN2A5c02pSyRg0.ouwGKfFE4aJslDrsLG4f0K88v8GWH3gDG')) {
+if (password_verify($_POST['password'], '$2y$10$QhS8hcs8frP7to.1tIw1OuQn2I2uJJNc.gcyVs2G1jQYn7vMqVNzq') && password_verify($_POST['username'], '$2y$10$rDQO8fN2A5c02pSyRg0.ouwGKfFE4aJslDrsLG4f0K88v8GWH3gDG')) {
     $_SESSION['admin'] = 'loggedIn';
     header('Location: dashboard.php');
 }
